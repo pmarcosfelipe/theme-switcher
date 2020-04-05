e7498c711688142d51c7d3bf801c66ee0a002d2a
Marcos Felipe <p.marcosfelipe@gmail.com>
2020-04-05 16:33:19 -0300

    Create a ThemeSwitcher using Styled-components, his ThemeContext, Typescript, Polished and create a Hook to get state in localStorage

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#333',
+    secondary: '#AF3B6E',
+
+    background: '#222',
+    text: '#fff'
+  }
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#AF3B6E',
+    secondary: '#BCE7FD',
+
+    background: '#F5F5F5',
+    text: '#333'
+  }
+};
